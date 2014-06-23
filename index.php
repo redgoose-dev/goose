cf5e3d88b4f514173801acdcfb22b40b819408e7
Redgoose <scripter@me.com>
2014-06-24 00:33:18 +0900

    에러레포팅 수정

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 header("content-type:text/html; charset=utf-8");
 session_cache_expire(30);
 session_start();
-error_reporting(E_ALL ^ E_NOTICE);
+error_reporting(E_ALL & ~E_NOTICE);
 
 define('GOOSE', true);
 define('PWD', dirname(__FILE__));
