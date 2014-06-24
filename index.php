7505cb8fedf46f0b16c8d79f1b99c4211331cdfe
Redgoose <scripter@me.com>
2014-06-25 05:50:12 +0900

    오류리포트 삭제, 체크인스톨 로드 삭제

--- index.php
+++ index.php
@@ -2,11 +2,9 @@
 header("content-type:text/html; charset=utf-8");
 session_cache_expire(30);
 session_start();
-error_reporting(E_ALL ^ E_NOTICE);
 
 define('GOOSE', true);
 define('PWD', dirname(__FILE__));
 
-require_once(PWD.'/libs/checkInstall.php');
 require_once(PWD.'/libs/init.php');
 ?>
