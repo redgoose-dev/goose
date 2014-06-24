7230afd9773d053cbe7affcf643b2b5514d07895
Redgoose <scripter@me.com>
2014-06-25 06:42:30 +0900

    git 에러로 수정

--- index.php
+++ index.php
@@ -2,10 +2,6 @@
 header("content-type:text/html; charset=utf-8");
 session_cache_expire(30);
 session_start();
-<<<<<<< HEAD
-error_reporting(E_ALL & ~E_NOTICE);
-=======
->>>>>>> dev-v0.3
 
 define('GOOSE', true);
 define('PWD', dirname(__FILE__));
