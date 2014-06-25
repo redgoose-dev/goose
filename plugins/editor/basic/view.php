a42ae2941f922f36e388882dc9696fdc0f2695c3
Redgoose <scripter@me.com>
2014-06-26 07:52:00 +0900

    일반글 태그표현
    아직 미완성…
    최소한 링크와 이미지만이라도 기능을 넣고싶은데 정규표현식으로 처리해야할거 같다.

--- plugins/editor/basic/view.php
+++ plugins/editor/basic/view.php
@@ -3,6 +3,9 @@ $path = ROOT.'/plugins/editor/'.$nest['editor'];
 
 $article['content'] = htmlspecialchars($article['content']);
 $article['content'] = nl2br($article['content']);
+
+$article['content'] = str_replace('[[', '<', $article['content']);
+$article['content'] = str_replace(']]', '>', $article['content']);
 ?>
 
 <link rel="stylesheet" href="<?=$path?>/css/basic.css" />
