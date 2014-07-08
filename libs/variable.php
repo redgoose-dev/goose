ba1649120b34be3d2d96e209145d49d145c7f8d9
Scripter <scripter@me.com>
2014-07-08 13:52:45 +0900

    jQuery 버전변경

--- libs/variable.php
+++ libs/variable.php
@@ -2,7 +2,7 @@
 if(!defined("GOOSE")){exit();}
 
 $extraKeyTypeArray = array("한줄 입력칸(text)", "여러 줄 입력칸(textarea)", "단일선택(select)");
-$jQueryAddress = 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js';
+$jQueryAddress = 'http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js';
 $dataOriginalDirectory = '/data/original/';
 $dataThumnailDirectory = '/data/thumnail/';
 ?>
