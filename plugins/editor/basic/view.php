9c66965b67af5a2e700a74685df3d5c52c2d6cdd
Redgoose <scripter@me.com>
2014-07-10 07:34:25 +0900

    css 일부 수정

--- plugins/editor/basic/view.php
+++ plugins/editor/basic/view.php
@@ -8,4 +8,9 @@ $article['content'] = str_replace('[[', '<', $article['content']);
 $article['content'] = str_replace(']]', '>', $article['content']);
 ?>
 
-<link rel="stylesheet" href="<?=$path?>/css/basic.css" />
+<style>
+.articleBody {
+	font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+	font-size: 14px; color: #333; line-height: 1.42857143;
+}
+</style>
