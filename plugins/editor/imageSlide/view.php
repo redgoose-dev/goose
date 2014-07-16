5ff3d6463ecaf267f3045b42bf9be470e04794f7
Redgoose <scripter@me.com>
2014-07-17 07:50:32 +0900

    view 페이지 작업

--- plugins/editor/imageSlide/view.php
+++ plugins/editor/imageSlide/view.php
@@ -3,12 +3,37 @@ $path = ROOT.'/plugins/editor/'.$nest['editor'];
 
 $article['content'] = urldecode($article['content']);
 $article['content'] = json_decode($article['content']);
-
-$util->console($article['content']);
 ?>
 
+<link rel="stylesheet" href="<?=$path?>/css/view.css" />
+
 <div class="articleBody">
-	<?
-	var_dump($article['content']);
-	?>
+	<ul>
+		<?
+		foreach ($article['content'] as $k=>$v)
+		{
+		?>
+			<li>
+				<div class="wrap">
+					<figure><img src="<?=ROOT?>/data/original/<?=$v->location?>" alt="" /></figure>
+					<div class="body">
+						<h3><?=$v->filename?></h3>
+						<?
+						foreach ($v->form as $k2=>$v2)
+						{
+						?>
+							<p>
+								<strong><?=$v2->key?></strong>
+								<span><?=$v2->value?></span>
+							</p>
+						<?
+						}
+						?>
+					</div>
+				</div>
+			</li>
+		<?
+		}
+		?>
+	</ul>
 </div>
