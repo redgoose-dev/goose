eb2f133cb9ce080566ec272b1ffe5f7f53f6b278
Scripter <scripter@me.com>
2014-07-25 11:55:04 +0900

    버전변경 v0.2.7

--- libs/variable.php
+++ libs/variable.php
@@ -1,7 +1,7 @@
 <?php
 if(!defined("GOOSE")){exit();}
 
-define('GOOSE_VERSION', '0.2.6');
+define('GOOSE_VERSION', '0.2.7');
 
 $extraKeyTypeArray = array("한줄 입력칸(text)", "여러 줄 입력칸(textarea)", "단일선택(select)");
 $jQueryAddress = 'http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js';
