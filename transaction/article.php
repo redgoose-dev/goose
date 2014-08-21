4f09b02a8dbeede057edd2b4043ce9a180982382
Diaho <scripter@me.com>
2014-08-22 07:47:49 +0900

    버전 0.3 개편작업

---
+++ transaction/article.php
@@ -0,0 +1,34 @@
+<?php
+if(!defined("GOOSE")){exit();}
+
+$ipAddress = $_SERVER['REMOTE_ADDR'];
+$regdate = date("YmdHis");
+$_POST['title'] = htmlspecialchars($_POST['title']);
+
+// get nest
+$nest = $goose->spawn->getItem(array(
+	'table' => 'nests',
+	'where' => 'srl='.$_POST['nest_srl']
+));
+try {
+	$nest['json'] = json_decode($nest['json'], true);
+} catch(Exception $e) {}
+
+
+// load skin file
+$skin = (isset($nest['json']['articleSkin'])) ? $nest['json']['articleSkin'] : 'basic';
+
+if (is_file(PWD.'/plugins/article/'.$skin.'/transaction.php'))
+{
+	require_once(PWD.'/plugins/article/'.$skin.'/transaction.php');
+}
+else if (is_file(PWD.'/plugins/article/basic/transaction.php'))
+{
+	require_once(PWD.'/plugins/article/basic/transaction.php');
+}
+else
+{
+	$goose->util->alert('불러올 페이지가 없습니다.');
+	$goose->out();
+}
+?>
