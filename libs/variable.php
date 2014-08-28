3c7ed3911985bb2d89d72d02bd1d33864dba8134
Redgoose <scripter@me.com>
2014-08-28 13:16:01 +0900

    Update variable.php

--- libs/variable.php
+++ libs/variable.php
@@ -1,7 +1,7 @@
 <?php
 if(!defined("GOOSE")){exit();}
 
-define('GOOSE_VERSION', '0.2.7');
+define('GOOSE_VERSION', '0.3');
 
 $jQueryAddress = 'http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js';
 $dataOriginalDirectory = '/data/original/';
