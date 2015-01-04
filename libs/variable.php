87fcd30446b986564f4bc3bb9d95c6a2fa473c06
redgoose <scripter@me.com>
2015-01-04 14:49:20 +0900

    update jQuery 2.1.3

--- libs/variable.php
+++ libs/variable.php
@@ -3,7 +3,7 @@ if(!defined("GOOSE")){exit();}
 
 define('GOOSE_VERSION', '0.3');
 
-$jQueryAddress = 'http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js';
+$jQueryAddress = 'http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js';
 $dataOriginalDirectory = '/data/original/';
 $dataThumnailDirectory = '/data/thumnail/';
 ?>
