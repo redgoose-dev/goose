76fb278ad63065cdd266d997e1d5c9ff17346a2d
Redgoose <scripter@me.com>
2015-02-23 08:43:47 +0900

    v0.3.5 Patched
    nest.php 수정

---
+++ transaction/nest.php
@@ -0,0 +1,34 @@
+<?php
+if(!defined("GOOSE")){exit();}
+
+
+// convert
+$json = (isset($_POST['json'])) ? $goose->util->jsonToArray($_POST['json']) : null;
+
+// load skin page
+$skin = (isset($json['skin'])) ? $json['skin'] : $goose->user['skinDefault'];
+$skin_path = '/plugins/nest/'.$skin;
+
+
+if (is_file(PWD.$skin_path.'/transaction.php'))
+{
+	require_once(PWD.$skin_path.'/transaction.php');
+	if ($redirectUrl)
+	{
+		$goose->util->redirect($redirectUrl);
+	}
+}
+else if (is_file(PWD.'/plugins/nest/'.$goose->user['skinDefault'].'/transaction.php'))
+{
+	require_once(PWD.'/plugins/nest/'.$goose->user['skinDefault'].'/transaction.php');
+	if ($redirectUrl)
+	{
+		$goose->util->redirect($redirectUrl);
+	}
+}
+else
+{
+	$goose->util->alert('불러올 페이지가 없습니다.');
+	$goose->out();
+}
+?>
