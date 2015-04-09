d6606506c186a74b212e7cc1a823256a1c8aaca4
Redgoose <scripter@me.com>
2015-04-09 14:17:01 +0900

    표시제한방식 수정
    권한이 안되면 목록에서 아예 빼버렸는데 표시되는 둥지숫자를 정확하게 계산할 수 없고 둥지가 없을때의 처리가 더 좋지않은 상황이어서
    표시 방식을 변경했다.

--- pages/nest_index.php
+++ pages/nest_index.php
@@ -14,7 +14,7 @@ if(!defined("GOOSE")){exit();}
 			$active = (!$group_srl) ? 'class="active"' : '';
 			?>
 			<li <?=$active?>>
-				<a href="<?=GOOSE_ROOT?>/nest/index/?all=1">All<?=($goose->isAdmin) ? "($nestsAllCount)" : ""?></a>
+				<a href="<?=GOOSE_ROOT?>/nest/index/?all=1">All(<?=$nestsAllCount?>)</a>
 			</li>
 			<?
 			$nestGroupsIndex = $goose->spawn->getItems(array(
@@ -24,18 +24,11 @@ if(!defined("GOOSE")){exit();}
 			));
 			foreach ($nestGroupsIndex as $k=>$v)
 			{
-				if ($goose->isAdmin)
-				{
-					$nestCount = $goose->spawn->getCount(array(
-						'table' => 'nests',
-						'where' => 'group_srl='.(int)$v['srl']
-					));
-					$nestCount = '('.$nestCount.')';
-				}
-				else
-				{
-					$nestCount = '';
-				}
+				$nestCount = $goose->spawn->getCount(array(
+					'table' => 'nests',
+					'where' => 'group_srl='.(int)$v['srl']
+				));
+				$nestCount = '('.$nestCount.')';
 
 				$active = ($group_srl == $v['srl']) ? " class='active'" : "";
 				$url = GOOSE_ROOT.'/nest/index/'.$v['srl'].'/';
@@ -54,59 +47,73 @@ if(!defined("GOOSE")){exit();}
 			foreach ($nestsIndex as $k=>$v)
 			{
 				$v['json'] = json_decode(urldecode($v['json']), true);
-				if ($v['json']['permission'] && $v['json']['permission'] < $_SESSION['gooseLevel'])
+				$allow = ($v['json']['permission'] && $v['json']['permission'] < $_SESSION['gooseLevel']) ? false : true;
+				if ($allow)
 				{
-					continue;
-				}
-				$url = GOOSE_ROOT.'/article/index/'.$v['srl'].'/';
-				$articleCount = $goose->spawn->getCount(array(
-					'table' => 'articles',
-					'where' => 'nest_srl='.(int)$v['srl']
-				));
-				$categoryCount = $goose->spawn->getCount(array(
-					'table' => 'categories',
-					'where' => 'nest_srl='.(int)$v['srl']
-				));
-				$categoryCount = ($categoryCount && $v['useCategory']==1) ? '<span>Category:'.$categoryCount.'</span>' : '';
-				$groupName = $goose->spawn->getItem(array(
-					'field' => 'name',
-					'table' => 'nestGroups',
-					'where' => 'srl='.(int)$v['group_srl']
-				));
-				$groupName = ($groupName['name']) ? "<em>[".$groupName['name']."]</em>" : "";
-				$categoryBtn = ($v['useCategory'] == 1) ? '<a href="'.GOOSE_ROOT.'/category/index/'.$v['srl'].'/">분류설정</a>' : '';
+					$url = GOOSE_ROOT.'/article/index/'.$v['srl'].'/';
+					$articleCount = $goose->spawn->getCount(array(
+						'table' => 'articles',
+						'where' => 'nest_srl='.(int)$v['srl']
+					));
+					$categoryCount = $goose->spawn->getCount(array(
+						'table' => 'categories',
+						'where' => 'nest_srl='.(int)$v['srl']
+					));
+					$categoryCount = ($categoryCount && $v['useCategory']==1) ? '<span>Category:'.$categoryCount.'</span>' : '';
+					$groupName = $goose->spawn->getItem(array(
+						'field' => 'name',
+						'table' => 'nestGroups',
+						'where' => 'srl='.(int)$v['group_srl']
+					));
+					$groupName = ($groupName['name']) ? "<em>[".$groupName['name']."]</em>" : "";
+					$categoryBtn = ($v['useCategory'] == 1) ? '<a href="'.GOOSE_ROOT.'/category/index/'.$v['srl'].'/">분류설정</a>' : '';
 		?>
-				<li>
-					<dl>
-						<dd>
-							<a href="<?=$url?>"><strong class="big"><?=$groupName?> <?=$v['name']?>(<?=$articleCount?>)</strong></a>
-							<div class="inf">
-								<span>ID:<?=$v['id']?></span>
-								<span>Date:<?=$goose->util->convertDate($v['regdate'])?></span>
-								<?=$categoryCount?>
-								<span>Level:<?=$v['json']['permission']?></span>
-							</div>
-							<?
-							if ($goose->isAdmin)
-							{
-							?>
-								<nav>
-									<a href="<?=GOOSE_ROOT?>/nest/modify/<?=$v['srl']?>/">수정</a>
-									<a href="<?=GOOSE_ROOT?>/nest/delete/<?=$v['srl']?>/">삭제</a>
-									<?=$categoryBtn?>
-								</nav>
-							<?
-							}
-							?>
-						</dd>
-					</dl>
-				</li>
+					<li>
+						<dl>
+							<dd>
+								<a href="<?=$url?>"><strong class="big"><?=$groupName?> <?=$v['name']?>(<?=$articleCount?>)</strong></a>
+								<div class="inf">
+									<span>ID:<?=$v['id']?></span>
+									<span>Date:<?=$goose->util->convertDate($v['regdate'])?></span>
+									<?=$categoryCount?>
+									<span>Level:<?=$v['json']['permission']?></span>
+								</div>
+								<?
+								if ($goose->isAdmin)
+								{
+								?>
+									<nav>
+										<a href="<?=GOOSE_ROOT?>/nest/modify/<?=$v['srl']?>/">수정</a>
+										<a href="<?=GOOSE_ROOT?>/nest/delete/<?=$v['srl']?>/">삭제</a>
+										<?=$categoryBtn?>
+									</nav>
+								<?
+								}
+								?>
+							</dd>
+						</dl>
+					</li>
 		<?
+				}
+				else
+				{
+		?>
+					<li>
+						<dl>
+							<dd>
+								<strong class="disabled">권한이 없는 둥지</strong>
+							</dd>
+						</dl>
+					</li>
+		<?
+				}
 			}
 		}
 		else
 		{
-			echo '<li class="empty">데이터가 없습니다.</li>';
+		?>
+			<li class="empty">데이터가 없습니다.</li>;
+		<?
 		}
 		?>
 	</ul>
