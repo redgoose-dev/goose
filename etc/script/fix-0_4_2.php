57f3cb6456149e7a9aebc024e0a34dcefa08cc24
Redgoose <scripter@me.com>
2015-04-12 02:24:37 +0900

    스크립트 데이터 변환부분 추가

--- etc/script/fix-0_4_2.php
+++ etc/script/fix-0_4_2.php
@@ -2,19 +2,19 @@
 // 필드수정
 $add_field_tempFiles = $goose->spawn->action("
 	ALTER TABLE `".$goose->tablesName['tempFiles']."`
-		ADD `type` VARCHAR(50) NULL DEFAULT NULL AFTER `name`,
+		ADD `type` VARCHAR(100) NULL DEFAULT NULL AFTER `name`,
 		ADD `size` BIGINT(11) NULL DEFAULT NULL AFTER `type`
 ");
 
 if($add_field_tempFiles)
 {
 	echo "<p>tempFiles - complete fix<p>";
+  echo "<hr>";
 }
-
 
 $add_field_files = $goose->spawn->action("
 	ALTER TABLE `".$goose->tablesName['files']."`
-		ADD `type` VARCHAR(50) NULL DEFAULT NULL AFTER `loc`,
+		ADD `type` VARCHAR(100) NULL DEFAULT NULL AFTER `loc`,
 		ADD `size` BIGINT(11) NULL DEFAULT NULL AFTER `type`,
 		ADD `date` VARCHAR(14) NULL DEFAULT NULL AFTER `size`
 ");
@@ -22,4 +22,102 @@ $add_field_files = $goose->spawn->action("
 if($add_field_files)
 {
 	echo "<p>files - complete fix<p>";
+  echo "<hr>";
 }
+
+
+// 기존 데이터들 새로운 필드의 데이터를 참고하여 데이터 수정하기
+$files = $goose->spawn->getItems(array(
+	'table' => $goose->tablesName['files']
+	,'where' => 'type is NULL'
+));
+
+foreach($files as $k=>$v)
+{
+	$article = $goose->spawn->getItem(array(
+		'table' => $goose->tablesName['articles']
+		,'field' => 'regdate'
+		,'where' => 'srl='.$v['article_srl']
+	));
+
+	$ext = substr(strrchr($v['name'], '.'), 1);
+	$type = null;
+	if ($ext == 'jpg')
+	{
+		$type = 'image/jpeg';
+	}
+	else if ($ext == 'png')
+	{
+		$type = 'image/png';
+	}
+	else if ($ext == 'gif')
+	{
+		$type = 'image/gif';
+	}
+	else if ($ext == 'svg')
+	{
+		$type = 'image/svg+xml';
+	}
+	else if ($ext == 'txt')
+	{
+		$type = 'text/plain';
+	}
+	else if ($ext == 'doc')
+	{
+		$type = 'application/msword';
+	}
+	else if ($ext == 'pdf')
+	{
+		$type = 'application/pdf';
+	}
+	else if ($ext == 'json')
+	{
+		$type = 'application/json';
+	}
+	else if ($ext == 'js')
+	{
+		$type = 'text/javascript';
+	}
+	else if ($ext == 'css')
+	{
+		$type = 'text/css';
+	}
+	else if ($ext == 'htm' || $ext == 'html')
+	{
+		$type = 'text/html';
+	}
+	else if ($ext == 'exe')
+	{
+		$type = 'application/octet-stream';
+	}
+	else if ($ext == 'zip')
+	{
+		$type = 'application/zip';
+	}
+	else if ($ext == 'rar')
+	{
+		$type = 'application/x-rar-compressed';
+	}
+
+	if (file_exists(PWD.'/data/original/'.$v['loc']))
+	{
+		$size = filesize(PWD.'/data/original/'.$v['loc']);
+	}
+	else
+	{
+		$size = 0;
+	}
+
+	$update = $goose->spawn->update(array(
+		'table' => $goose->tablesName['files'],
+		'where' => 'srl='.$v['srl'],
+		'data' => array(
+			"type='$type'"
+			,"size=$size"
+			,"date='".$article['regdate']."'"
+		)
+		,'debug' => false
+	));
+}
+echo "update database";
+echo "<hr>";
