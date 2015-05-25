5acef8b5f332dd6491eb8829db282d6b42053591
Redgoose <scripter@me.com>
2015-05-26 02:03:15 +0900

    remove files

--- README.md
+++
@@ -1,4 +0,0 @@
-Goose Reboot
-=====
-
-전체적으로 다 뜯어고치는 프로젝트 Goose reboot
--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Copyright Redgoose <http://redgoose.me> */
-
-define('__GOOSE__', true);
-define('__GOOSE_DEBUG__', true);
-
-require_once dirname(__FILE__) . '/core/init.php';
