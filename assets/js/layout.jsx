b58418f5f319ac5492982392ae748bd6e02b2511
Redgoose <scripter@me.com>
2015-05-28 09:18:40 +0900

    페이지 스크립트 삽질

--- assets/js/layout.jsx
+++ assets/js/layout.jsx
@@ -1,19 +1,45 @@
+var TopNavigationItems = React.createClass({
+	render : function()
+	{
+		if (!this.props.data) return false;
+		var items = this.props.data.map(function(item){
+			return <li><a href={item.url}>{item.name}</a></li>;
+		});
+		return <ul>{items}</ul>;
+	}
+});
+
 var TopNavigation = React.createClass({
-	index : [1, 2, 3],
+	getInitialState : function()
+	{
+		return {
+			navigationData : {}
+		};
+	},
+
+	componentDidMount : function()
+	{
+		$.get(this.props.source, function(result){
+			if (this.isMounted())
+			{
+				this.setState({
+					navigationData : result
+				});
+			}
+		}.bind(this));
+	},
+
 	render : function(){
 		return (
 			<nav className="lay-top-navigation">
-				<ul>
-					<li><a href="#">aaaa</a></li>
-					<li><a href="#">bbb</a></li>
-					<li><a href="#">cccc</a></li>
-				</ul>
+				<TopNavigationItems data={this.state.navigationData.top}/>
 			</nav>
 		);
 	}
 });
 
+// render top navigation
 React.render(
-	<TopNavigation />,
+	<TopNavigation type="top" source="./data/navigation.json" />,
 	document.getElementById('comp-top-navigation')
 );
