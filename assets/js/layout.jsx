73c56131918cfb90abc357130c7a796510706304
Redgoose <scripter@me.com>
2015-05-28 13:18:54 +0900

    네비게이션 부분 작업

--- assets/js/layout.jsx
+++ assets/js/layout.jsx
@@ -1,14 +1,3 @@
-var TopNavigationItems = React.createClass({
-	render : function()
-	{
-		if (!this.props.data) return false;
-		var items = this.props.data.map(function(item){
-			return <li><a href={item.url}>{item.name}</a></li>;
-		});
-		return <ul>{items}</ul>;
-	}
-});
-
 var TopNavigation = React.createClass({
 	getInitialState : function()
 	{
@@ -16,30 +5,50 @@ var TopNavigation = React.createClass({
 			navigationData : {}
 		};
 	},
-
-	componentDidMount : function()
-	{
-		$.get(this.props.source, function(result){
-			if (this.isMounted())
-			{
-				this.setState({
-					navigationData : result
-				});
-			}
-		}.bind(this));
-	},
-
+	componentDidMount : function() {},
 	render : function(){
 		return (
 			<nav className="lay-top-navigation">
-				<TopNavigationItems data={this.state.navigationData.top}/>
+				<ul>
+					{this.props.source.map(function(item){
+						return <li key={item.name}><a href={item.url} target={item.target}>{item.name}</a></li>;
+					})}
+				</ul>
 			</nav>
 		);
 	}
 });
 
-// render top navigation
-React.render(
-	<TopNavigation type="top" source="./data/navigation.json" />,
-	document.getElementById('comp-top-navigation')
-);
+var SideNavigation = React.createClass({
+	getInitialState : function()
+	{
+		return {
+			navigationData : {}
+		};
+	},
+	componentDidMount : function() {},
+	render : function(){
+		return (
+			<nav className="lay-side-navigation">
+				<ul>
+					<li>aaaa</li>
+				</ul>
+			</nav>
+		);
+	}
+});
+
+// load navigation data
+$.get('./data/navigation.json', function(data){
+	// render top navigation
+	React.render(
+		<TopNavigation type="top" source={data} />,
+		document.getElementById('comp-top-navigation')
+	);
+
+	// render side navigation
+	React.render(
+		<SideNavigation source={data} />,
+		document.getElementById('comp-side-navigation')
+	);
+});
