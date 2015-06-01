2a26b2f7369940b3df0ad71b43844ee0898e1b7f
Redgoose <scripter@me.com>
2015-06-01 09:14:13 +0900

    소개페이지 작업

--- assets/js/build/layout.js
+++ assets/js/build/layout.js
@@ -1,100 +1,211 @@
-var navigationData = null;
-
-var TopNavigationList = React.createClass({displayName: "TopNavigationList",
-	render : function()
+/**
+ * Object to Array
+ * object형식의 데이더를 array형식의 데이터로 변환시킨다. (only 1depth)
+ *
+ * @param {Object} obj
+ * @return {Array}
+ */
+var objectToArray = function(obj)
+{
+	var result = [];
+	for (var o in obj)
 	{
-		if (this.props.data)
+		result.push(obj[o]);
+	}
+	return result;
+};
+
+
+/**
+ * Navigation
+ *
+ * @param {String} url navigation map file url
+ */
+function Navigation(url)
+{
+	var self = this;
+	this.data = null;
+	this.page = null;
+
+
+	/**
+	 * COMPONENT AREA
+	 */
+
+	// top
+	this.CompTop = React.createClass({displayName: "CompTop",
+		getInitialState : function()
 		{
-			// 변환된 배열로 출력하기
-			return React.createElement("ul", null);
+			return { navData : null };
+		},
+		componentDidMount : function()
+		{
+			var top = this;
+
+			// load navigation data
+			$.get(this.props.sourceUrl, function(data){
+				self.data = data;
+				top.setState({ navData : objectToArray(data) });
+				$(window).trigger('hashchange');
+			});
+		},
+
+		render : function()
+		{
+			return (
+				React.createElement("nav", {className: "lay-top-navigation"}, 
+					React.createElement("h1", null, "top navigation"), 
+					React.createElement(self.CompTopList, {data: this.state.navData})
+				)
+			);
+		}
+	});
+
+	// top list
+	this.CompTopList = React.createClass({displayName: "CompTopList",
+		render : function()
+		{
+			if (!this.props.data) return React.createElement("ul", null);
+			var items = this.props.data.map(function(data){
+				return React.createElement("li", {key: data.name}, React.createElement("a", {href: data.url, target: data.target}, data.name));
+			});
+			return React.createElement("ul", null, items);
+		}
+	});
+
+	// side
+	this.CompSide = React.createClass({displayName: "CompSide",
+		getInitialState : function()
+		{
+			return { items : null };
+		},
+		update : function(obj)
+		{
+			this.setState({ items : obj });
+		},
+		componentDidMount : function() {},
+		render : function(){
+			return (
+				React.createElement("nav", {className: "lay-side-navigation"}, 
+					React.createElement(self.CompSideList, {data: this.state.items})
+				)
+			);
+		}
+	});
+
+	// side list
+	this.CompSideList = React.createClass({displayName: "CompSideList",
+		render : function(){
+			if (!this.props.data) return React.createElement("ul", null);
+			var items = this.props.data.map(function(data){
+				return React.createElement("li", {key: data.name}, React.createElement("a", {href: '#' + data.url}, data.name));
+			});
+			return React.createElement("ul", null, items);
+		}
+	});
+
+
+	/**
+	 * EVENT AREA
+	 */
+
+	// change hash event
+	$(window).on('hashchange', function() {
+		if (location.hash)
+		{
+			var params = location.hash.replace(/^#/gi, '').split('/');
+			if (self.data[params[0]] && (params[0] !== self.page))
+			{
+				var item = self.data[params[0]];
+				var children = null;
+				self.page = params[0];
+
+				// set children from navigation data
+				if (item['child'])
+				{
+					children = item['child'];
+				}
+
+				// load page
+				var url = './pages/' + self.page + '.html';
+				$('#contents').load(url, function(res, state, req){
+					if (state == 'error')
+					{
+						// not found
+						log(state + ': ' + req.statusText);
+					}
+					else
+					{
+						// get side navigation data
+						var $contentsGroup = $(this).find('[data-content-group]');
+						if ($contentsGroup.length)
+						{
+							children = [];
+							$contentsGroup.find('[data-content]').each(function(){
+								var id = $(this).attr('id');
+								var name = $(this).children('h1').text();
+								children.push({
+									name : name,
+									url : self.page + '/' + id
+								});
+							});
+						}
+
+						// update side navigation
+						self.side.update(children);
+
+						// go to scroll
+						if (params[1])
+						{
+							contents.gotoScroll(params[1]);
+						}
+					}
+				});
+			}
+			else if (params[1])
+			{
+				contents.gotoScroll(params[1]);
+			}
 		}
 		else
 		{
-			return React.createElement("ul", null);
+			// not found
 		}
-
-		//var items = this.props.data.map(function(data){
-		//	return <li key={data.name}><a href={data.url} target={data.target}>{data.name}</a></li>;
-		//});
-		//return <ul>{items}</ul>;
-	}
-});
-
-var TopNavigation = React.createClass({displayName: "TopNavigation",
-	getInitialState : function()
-	{
-		return { navData : null };
-	},
-	componentDidMount : function()
-	{
-		var self = this;
-
-		// change hash event
-		$(window).on('hashchange', function() {
-			//log(location.hash);
-			compNavigationSide.update('Documentation');
-		});
-
-		// load navigation data
-		$.get(this.props.sourceUrl, function(data){
-			navigationData = data;
-			// 1dep메뉴 데이터는 배열로 변환하기
-			self.setState({ navData : data });
-
-			$(window).trigger('hashchange');
-		});
-	},
-
-	render : function()
-	{
-		return (
-			React.createElement("nav", {className: "lay-top-navigation"}, 
-				React.createElement(TopNavigationList, {data: this.state.navData})
-			)
-		);
-	}
-});
+	});
 
 
-//{this.props.source.map(function(item){
-//	return <li key={item.name}><a href={item.url} target={item.target}>{item.name}</a></li>;
-//})}
+	/**
+	 * RENDER AREA
+	 */
 
-var SideNavigation = React.createClass({displayName: "SideNavigation",
-	getInitialState : function()
-	{
-		return {
-			items : null
-		};
-	},
-	update : function(key)
-	{
-		//log(navigationData);
-		//log(key);
-	},
-	componentDidMount : function() {},
-	render : function(){
-		return (
-			React.createElement("nav", {className: "lay-side-navigation"}, 
-				React.createElement("ul", null, this.state.items)
-			)
-		);
-	}
-});
+	// top
+	this.top = React.render(
+		React.createElement(this.CompTop, {sourceUrl: "./data/navigation.json"}),
+		document.getElementById('comp-top-navigation')
+	);
+
+	// side
+	this.side = React.render(
+		React.createElement(this.CompSide, {key: "side"}),
+		document.getElementById('comp-side-navigation')
+	);
+
+}
 
 
-/*
- * Render components
+/**
+ * Contents
  */
+function Contents()
+{
+	this.gotoScroll = function(target)
+	{
+		if (target)
+		{
+			$('html, body').animate({ scrollTop: $('#' + target).offset().top }, '300');
+		}
+	}
+}
 
-// render side navigation
-var compNavigationSide = React.render(
-	React.createElement(SideNavigation, {key: "side", foo: this.bar}),
-	document.getElementById('comp-side-navigation')
-);
-
-// render top navigation
-var compNavigationTop = React.render(
-	React.createElement(TopNavigation, {sourceUrl: "./data/navigation.json"}),
-	document.getElementById('comp-top-navigation')
-);
+var navigation = new Navigation('./data/navigation.json');
+var contents = new Contents();
--- assets/js/src/layout.jsx
+++ assets/js/src/layout.jsx
@@ -1,5 +1,3 @@
-var navigationData = null;
-
 /**
  * Object to Array
  * object형식의 데이더를 array형식의 데이터로 변환시킨다. (only 1depth)
@@ -17,90 +15,197 @@ var objectToArray = function(obj)
 	return result;
 };
 
-// top navigation items
-var TopNavigationList = React.createClass({
-	render : function()
-	{
-		if (!this.props.data) return <ul/>;
-		var items = this.props.data.map(function(data){
-			return <li key={data.name}><a href={data.url} target={data.target}>{data.name}</a></li>;
-		});
-		return <ul>{items}</ul>;
-	}
-});
 
-// top navigation wrap
-var TopNavigation = React.createClass({
-	getInitialState : function()
-	{
-		return { navData : null };
-	},
-	componentDidMount : function()
-	{
-		var self = this;
-
-		// change hash event
-		$(window).on('hashchange', function() {
-			//log(location.hash);
-			compNavigationSide.update('Documentation');
-		});
-
-		// load navigation data
-		$.get(this.props.sourceUrl, function(data){
-			navigationData = data;
-			self.setState({ navData : objectToArray(data) });
-			$(window).trigger('hashchange');
-		});
-	},
-
-	render : function()
-	{
-		return (
-			<nav className="lay-top-navigation">
-				<TopNavigationList data={this.state.navData} />
-			</nav>
-		);
-	}
-});
-
-// side navigation
-var SideNavigation = React.createClass({
-	getInitialState : function()
-	{
-		return {
-			items : null
-		};
-	},
-	update : function(key)
-	{
-		//log(navigationData);
-		log(navigationData[key]);
-
-		// 페이지 호출하기
-	},
-	componentDidMount : function() {},
-	render : function(){
-		return (
-			<nav className="lay-side-navigation">
-				<ul>{this.state.items}</ul>
-			</nav>
-		);
-	}
-});
-
-
-/*
- * Render components
+/**
+ * Navigation
+ *
+ * @param {String} url navigation map file url
  */
+function Navigation(url)
+{
+	var self = this;
+	this.data = null;
+	this.page = null;
 
-// render side navigation
-var compNavigationSide = React.render(
-	<SideNavigation key="side" foo={this.bar} />,
-	document.getElementById('comp-side-navigation')
-);
 
-// render top navigation
-var compNavigationTop = React.render(
-	<TopNavigation sourceUrl="./data/navigation.json" />,
-	document.getElementById('comp-top-navigation')
-);
+	/**
+	 * COMPONENT AREA
+	 */
+
+	// top
+	this.CompTop = React.createClass({
+		getInitialState : function()
+		{
+			return { navData : null };
+		},
+		componentDidMount : function()
+		{
+			var top = this;
+
+			// load navigation data
+			$.get(this.props.sourceUrl, function(data){
+				self.data = data;
+				top.setState({ navData : objectToArray(data) });
+				$(window).trigger('hashchange');
+			});
+		},
+
+		render : function()
+		{
+			return (
+				<nav className="lay-top-navigation">
+					<h1>top navigation</h1>
+					<self.CompTopList data={this.state.navData} />
+				</nav>
+			);
+		}
+	});
+
+	// top list
+	this.CompTopList = React.createClass({
+		render : function()
+		{
+			if (!this.props.data) return <ul/>;
+			var items = this.props.data.map(function(data){
+				return <li key={data.name}><a href={data.url} target={data.target}>{data.name}</a></li>;
+			});
+			return <ul>{items}</ul>;
+		}
+	});
+
+	// side
+	this.CompSide = React.createClass({
+		getInitialState : function()
+		{
+			return { items : null };
+		},
+		update : function(obj)
+		{
+			this.setState({ items : obj });
+		},
+		componentDidMount : function() {},
+		render : function(){
+			return (
+				<nav className="lay-side-navigation">
+					<self.CompSideList data={this.state.items} />
+				</nav>
+			);
+		}
+	});
+
+	// side list
+	this.CompSideList = React.createClass({
+		render : function(){
+			if (!this.props.data) return <ul/>;
+			var items = this.props.data.map(function(data){
+				return <li key={data.name}><a href={'#' + data.url}>{data.name}</a></li>;
+			});
+			return <ul>{items}</ul>;
+		}
+	});
+
+
+	/**
+	 * EVENT AREA
+	 */
+
+	// change hash event
+	$(window).on('hashchange', function() {
+		if (location.hash)
+		{
+			var params = location.hash.replace(/^#/gi, '').split('/');
+			if (self.data[params[0]] && (params[0] !== self.page))
+			{
+				var item = self.data[params[0]];
+				var children = null;
+				self.page = params[0];
+
+				// set children from navigation data
+				if (item['child'])
+				{
+					children = item['child'];
+				}
+
+				// load page
+				var url = './pages/' + self.page + '.html';
+				$('#contents').load(url, function(res, state, req){
+					if (state == 'error')
+					{
+						// not found
+						log(state + ': ' + req.statusText);
+					}
+					else
+					{
+						// get side navigation data
+						var $contentsGroup = $(this).find('[data-content-group]');
+						if ($contentsGroup.length)
+						{
+							children = [];
+							$contentsGroup.find('[data-content]').each(function(){
+								var id = $(this).attr('id');
+								var name = $(this).children('h1').text();
+								children.push({
+									name : name,
+									url : self.page + '/' + id
+								});
+							});
+						}
+
+						// update side navigation
+						self.side.update(children);
+
+						// go to scroll
+						if (params[1])
+						{
+							contents.gotoScroll(params[1]);
+						}
+					}
+				});
+			}
+			else if (params[1])
+			{
+				contents.gotoScroll(params[1]);
+			}
+		}
+		else
+		{
+			// not found
+		}
+	});
+
+
+	/**
+	 * RENDER AREA
+	 */
+
+	// top
+	this.top = React.render(
+		<this.CompTop sourceUrl="./data/navigation.json" />,
+		document.getElementById('comp-top-navigation')
+	);
+
+	// side
+	this.side = React.render(
+		<this.CompSide key="side" />,
+		document.getElementById('comp-side-navigation')
+	);
+
+}
+
+
+/**
+ * Contents
+ */
+function Contents()
+{
+	this.gotoScroll = function(target)
+	{
+		if (target)
+		{
+			$('html, body').animate({ scrollTop: $('#' + target).offset().top }, '300');
+		}
+	}
+}
+
+var navigation = new Navigation('./data/navigation.json');
+var contents = new Contents();
