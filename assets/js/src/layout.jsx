049205883fd6d42de08ae4d79a53dd19300440f2
Redgoose <scripter@me.com>
2015-06-02 09:15:55 +0900

    본격 페이지 작업

--- assets/js/src/layout.jsx
+++ assets/js/src/layout.jsx
@@ -111,65 +111,58 @@ function Navigation(url)
 
 	// change hash event
 	$(window).on('hashchange', function() {
-		if (location.hash)
+		var params = (location.hash) ? location.hash.replace(/^#/gi, '').split('/') : ['Introduce'];
+		if (self.data[params[0]] && (params[0] !== self.page))
 		{
-			var params = location.hash.replace(/^#/gi, '').split('/');
-			if (self.data[params[0]] && (params[0] !== self.page))
-			{
-				var item = self.data[params[0]];
-				var children = null;
-				self.page = params[0];
+			var item = self.data[params[0]];
+			var children = null;
+			self.page = params[0];
 
-				// set children from navigation data
-				if (item['child'])
+			// set children from navigation data
+			if (item['child'])
+			{
+				children = item['child'];
+			}
+
+			// load page
+			var url = './pages/' + self.page + '.html';
+			$('#contents').load(url, function(res, state, req){
+				if (state == 'error')
 				{
-					children = item['child'];
+					// not found
+					log(state + ': ' + req.statusText);
 				}
-
-				// load page
-				var url = './pages/' + self.page + '.html';
-				$('#contents').load(url, function(res, state, req){
-					if (state == 'error')
+				else
+				{
+					// get side navigation data
+					var $contentsGroup = $(this).find('[data-content-group]');
+					if ($contentsGroup.length)
 					{
-						// not found
-						log(state + ': ' + req.statusText);
-					}
-					else
-					{
-						// get side navigation data
-						var $contentsGroup = $(this).find('[data-content-group]');
-						if ($contentsGroup.length)
-						{
-							children = [];
-							$contentsGroup.find('[data-content]').each(function(){
-								var id = $(this).attr('id');
-								var name = $(this).children('h1').text();
-								children.push({
-									name : name,
-									url : self.page + '/' + id
-								});
+						children = [];
+						$contentsGroup.find('[data-content]').each(function(){
+							var id = $(this).attr('id');
+							var name = $(this).children('h1').text();
+							children.push({
+								name : name,
+								url : self.page + '/' + id
 							});
-						}
-
-						// update side navigation
-						self.side.update(children);
-
-						// go to scroll
-						if (params[1])
-						{
-							contents.gotoScroll(params[1]);
-						}
+						});
 					}
-				});
-			}
-			else if (params[1])
-			{
-				contents.gotoScroll(params[1]);
-			}
+
+					// update side navigation
+					self.side.update(children);
+
+					// go to scroll
+					if (params[1])
+					{
+						contents.gotoScroll(params[1]);
+					}
+				}
+			});
 		}
-		else
+		else if (params[1])
 		{
-			// not found
+			contents.gotoScroll(params[1]);
 		}
 	});
 
