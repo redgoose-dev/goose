46589935ff59be770a73933d59bcd008f9a13f9b
Redgoose <scripter@me.com>
2015-07-10 11:22:29 +0900

    인트로 모듈 클래스 파일에서 스킨으로 소스 일부분 이동

--- module/intro/intro.class.php
+++ module/intro/intro.class.php
@@ -36,36 +36,6 @@ class Intro {
 		// create layout module
 		$this->layout = Module::load('layout');
 
-		// set repo
-		$repo = Array('article');
-
-		// load modules
-		$article = Module::load('article');
-		$nest = Module::load('nest');
-		$category = Module::load('category');
-
-		// get articles
-		$data = $article->getItems(array( 'limit' => array(0, $this->set['pagePerCount']) ));
-		$repo['article'] = ($data['state'] == 'success') ? $data['data'] : array();
-
-		foreach($repo['article'] as $k=>$v)
-		{
-			$data = $nest->getItem(array(
-				'field' => 'name,json',
-				'where' => 'srl='.$v['nest_srl']
-			));
-			$repo['article'][$k]['nest'] = ($data['state'] == 'success') ? $data['data'] : null;
-
-			if ($repo['article'][$k]['nest']['json']['useCategory'] && $v['category_srl'])
-			{
-				$data = $category->getItem(array(
-					'field' => 'name',
-					'where' => 'srl='.(int)$v['category_srl']
-				));
-				$repo['article'][$k]['categoryName'] = ($data['state'] == 'success') ? $data['data']['name'] : null;
-			}
-		}
-
 		// set pwd_container
 		$this->pwd_container = __GOOSE_PWD__.$this->skinPath.'view_index.html';
 
