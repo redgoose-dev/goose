31453374e16328a6ecb50356c1f7ae925ddfed9d
Redgoose <scripter@me.com>
2015-09-09 14:59:20 +0900

    scss 빌드방식 변경

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var scss = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+
+
+var source = {
+	layout : './module/layout'
+};
+
+// convert sass to css
+gulp.task('layout-scss', function(){
+	gulp.src([
+		source.layout + '/**/layout.scss'
+	])
+		.pipe(sourcemaps.init())
+		.pipe(scss({
+			//outputStyle: 'compact'
+			outputStyle: 'compressed'
+		}).on('error', scss.logError))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(source.layout))
+	;
+});
+// set watcher scss
+gulp.task('layout-scss:watch', function(){
+	gulp.watch(files.scss, ['scss']);
+});
+
+
+// default
+gulp.task('default', function(){
+	console.log('say hello');
+});
