dfa44eb30a9735d690e8ccdcdd5cb58220938ab0
redgoose <scripter@me.com>
2015-10-08 12:13:08 +0900

    json 버그픽스

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
+function log(o){console.log(o);}
+
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var scss = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
+
+
+// get parameter
+var getParams = function(optionKey)
+{
+	var o = process.argv.indexOf("--" + optionKey);
+	return process.argv[o+1];
+};
 
 
 var source = {
@@ -26,6 +37,23 @@ gulp.task('layout-scss', function(){
 // set watcher scss
 gulp.task('layout-scss:watch', function(){
 	gulp.watch(source.layout + '/**/layout.scss', ['layout-scss']);
+});
+
+
+// compress javascript
+gulp.task('javascript', function(){
+	if (!getParams('option'))
+	{
+		log('please parameter "--option [javascript location]"');
+		return false;
+	}
+
+	var path = getParams('option').replace(/[^\/]*$/, '');
+
+	return gulp.src(getParams('option'))
+		.pipe(uglify())
+		.pipe(rename({ extname : '.min.js' }))
+		.pipe(gulp.dest(path));
 });
 
 
