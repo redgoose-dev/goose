61ab4e540c84a7a7830c3e778868d1ba767287ce
redgoose <scripter@me.com>
2018-05-03 07:56:13 +0900

    구상중..

--- mod/api/api.class.php
+++ mod/api/api.class.php
@@ -20,8 +20,22 @@ class api {
 		// method: GET, POST, PUT, PATCH
 
 		// * GET
-		// `/goose/api/mod/nest`
-		// `/goose/api/mod/article
+		// `/goose/API/`: 문서
+		// `/goose/API/nest/`: nest index
+		// `/goose/API/nest/1/`: nest srl:1 article
+		// `/goose/API/nest/?`: param
+
+		if ($this->params['action'])
+		{
+			$asd = 'qwe';
+		}
+		else
+		{
+			// 문서나 설정
+			$aa = '123';
+		}
+
+		core\Util::console($this->params['action']);
 
 		echo "api module";
 	}
