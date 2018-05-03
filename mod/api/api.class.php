d0b68ba797d94b31ab8e565404608338092eb7ea
redgoose <scripter@me.com>
2018-05-03 19:38:00 +0900

    구상중..

--- mod/api/api.class.php
+++ mod/api/api.class.php
@@ -19,14 +19,34 @@ class api {
 	{
 		// method: GET, POST, PUT, PATCH
 
-		// * GET
-		// `/goose/API/`: 문서
-		// `/goose/API/nest/`: nest index
-		// `/goose/API/nest/1/`: nest srl:1 article
-		// `/goose/API/nest/?`: param
+		// * action
+		// `/goose/api/`: 문서
+		// `/goose/api/nest/`: nest index
+		// `/goose/api/nest/1/`: nest srl:1 article
+
+		// * params
+		// `field=srl,name`: field
+		// `page=2`
+		// `size=10`
+
+		// * response
+		// {
+		//   code: 200,
+		//   data: null,
+		//   nav: {
+		//     page, // 현재 페이지 번호
+		//     count, // 전체 글 갯수
+		//     lastPage, // 마지막 페이지(필요할까 고민됨)
+		//     prev, // (srl) 이전글 번호
+		//     next // (srl) 다음글 번호
+		//   }
+		// }
 
 		if ($this->params['action'])
 		{
+			// TODO: 사용 가능한 모듈인지 검사
+			// TODO: 오직 json으로만 출력
+			core\Util::console($this->set['modules']);
 			$asd = 'qwe';
 		}
 		else
@@ -35,7 +55,7 @@ class api {
 			$aa = '123';
 		}
 
-		core\Util::console($this->params['action']);
+		core\Util::console('action:: ' . $this->params['action']);
 
 		echo "api module";
 	}
